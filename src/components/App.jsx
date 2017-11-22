f7f471074f407d90259d8bf5a1970d85db7db0a9
nishina555 <toshiharu.nishina@spacemarket.co.jp>
2017-11-23 07:25:42 +0900

    create components directory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-export class Hello extends React.Component {
+export class App extends React.Component {
   render (){
     return <h1>Hello!</h1>
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Hello} from './Hello'
+import {App} from './components/App'
 
 ReactDOM.render(
-  <Hello />,
+  <App />,
   document.getElementById('root'))
