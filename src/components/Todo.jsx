7b89e3fb2e0aaecf66ba14d217b4516008ace860
nishina <n.toshi.1.014@gmail.com>
2017-11-25 05:49:15 +0900

    create comple function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,26 +1,44 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Todo = props => (
-  <li>
-    {props.title}
-  </li>
-);
+// const Todo = props => (
+//   <li>
+//     {props.title}
+//   </li>
+// );
 
-// class Todo extends Component {
-//   render() {
-//     return (
-//       <li>
-//         {this.props.title}
-//       </li>
-//     );
-//   }
-// }
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title,
+      done: this.props.done,
+    };
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      done: !this.state.done,
+    });
+  }
+  render() {
+    const link = this.state.done ? 'Undone' : 'Done';
+    const style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+    };
+
+    return (
+      <li style={style} onClick={this.handleClick.bind(this)}>
+        {this.state.title}
+      </li>
+    );
+  }
+}
 
 Todo.propTypes = {
   title: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired,
 };
-
 
 export default Todo;
